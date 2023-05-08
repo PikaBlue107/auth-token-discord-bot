f679474fc58cc1fb0001f4f6dd52b735a95a7959
Melody Griesen <pikablue107@gmail.com>
2023-05-07 21:17:34 -0400

    docs/refactor/log: tweaks for readability & usability

--- bot.py
+++ bot.py
@@ -23,11 +23,8 @@ MAGIC_TRIGGER_STRING = "Take it away, Link Bot!"
 LOGS_DIR = 'logs'
 AUTH_FILE = f'{LOGS_DIR}/auth.log'
 LOG_FILE = f'{LOGS_DIR}/main.log'
-ERR_FILE = f'{LOGS_DIR}/err.log'
+# errors are printed only to console by default, and I'm too lazy to do it any other way
 PREFILL_FORM_LINK_TEMPLATE = 'https://docs.google.com/forms/d/e/1FAIpQLSc6_HtfblPc_hikKztWNh6SfEhKAEzFxTgUQqbFDXQ7qFq08A/viewform?usp=pp_url&entry.1426369734={username}&entry.1675772246={userid}&entry.1231032926={auth_token}'
-SPECIAL_MESSAGE = f"""
-Thanks <@{MAGIC_USER_ID}>, and hi @everyone! React to this message, and I'll DM you an authenticated link to fill out the consent form.
-"""
 
 # client setup
 intents = discord.Intents.default()
@@ -47,10 +44,19 @@ class UserAuth:
     nonce: int
     hash_digest: str = None
 
+
     def get_hash_data_str(self) -> str:
+        """Data string used to compute hash.
+        
+        <username>,<userid>,<timestamp>,<nonce>"""
+
         return f'{self.discord_username},{self.discord_userid},{self.timestamp},{self.nonce}'
 
+
     def get_full_auth_str(self) -> str:
+        """Data string + digest.
+        
+        <username>,<userid>,<timestamp>,<nonce>,<digest>"""
         return f'{self.get_hash_data_str()},{self.hash_digest}'
 
 
@@ -65,18 +71,24 @@ def get_token(discord_username: str, discord_userid: int) -> UserAuth:
 
 
 async def send_user_authenticated_link(user: discord.User):
+
+    # get user info, and log
     discord_username = f'{user.name}#{user.discriminator}'
     discord_userid = user.id
     log(f"starting authentication request for {discord_username} (id={user.id})")
+
+    # generate auth token, and log to auth file
     auth_token = get_token(discord_username=discord_username, discord_userid=discord_userid)
     log(auth_token.get_full_auth_str(), filename=AUTH_FILE)
 
+    # get pre-filled form link with user name/id and auth token
     resolved_prefill_form_link = PREFILL_FORM_LINK_TEMPLATE.format(
         username=urllib.parse.quote(discord_username),
         userid=discord_userid,
         auth_token=auth_token.hash_digest
     )
 
+    # send user a message with pre-filled form link
     await user.send(f"""
 Authenticated as <@{discord_userid}>.
 security stuff, if you're curious: ||userid={discord_userid}, epoch timestamp={auth_token.timestamp}, nonce={auth_token.nonce}, generated auth token={auth_token.hash_digest}
@@ -88,19 +100,25 @@ To get a new URL, either send me a message, or react on any of my messages!
 
 # utils
 def log(message: str, filename: str=LOG_FILE, print_dest=sys.stdout):
-    # create log folder if not yet created
+    """Prepend a timestamp and print message to console and file"""
+
+    # create log folder *once* on startup
     if not log.path_verified:
         Path(LOGS_DIR).mkdir(parents=True, exist_ok=True)
         log.path_verified = True
-    # get fully formatted message
-    now = datetime.now()
-    full_msg = f'{now.isoformat()}: {message}'
+
+    # get timestamp
+    now = datetime.now().isoformat()
+
     # print message to console
     if print_dest:
-        print(full_msg, file=print_dest)
+        print(f"{now} ({filename}): {message}", file=print_dest)
+
     # log message to file
     with open(filename, 'a') as f:
-        f.write(full_msg + '\n')
+        f.write(f"{now}: {message}\n")
+
+# ensure log folder exists *once* on startup
 log.path_verified = False
 
 
@@ -119,9 +137,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
+    """Respond to DMs, and magic messages in guilds."""
+
     # don't respond to my own messages
     if message.author == client.user:
         return
+    
     # if in DM
     if not message.guild:
         user: discord.User = message.author
@@ -134,17 +155,21 @@ async def on_message(message: discord.Message):
         if member.id == MAGIC_USER_ID and MAGIC_TRIGGER_STRING in message.content:
             # print the special message
             log(f"Detected the magic trigger from the magic user. Sending special message!")
-            await message.reply(content=SPECIAL_MESSAGE)
+            await message.reply(content=f"Thanks <@{MAGIC_USER_ID}>, and hi @everyone! React to this message, and I'll DM you an authenticated link to fill out the consent form.")
         
 
 @client.event
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    """Respond to reactions on *my* messages by sending an authenticated link in DMs."""
+
     # get message
     channel = await client.fetch_channel(int(payload.channel_id))
     message = await channel.fetch_message(int(payload.message_id))
+
     # if I'm not the author of the message, don't do anything
     if message.author != client.user:
         return
+    
     # get the user and send them an authenticated link
     user = await client.fetch_user(int(payload.user_id))
     log(f"observed reaction on my post from {user.name}#{user.discriminator}, sending authenticated link")
